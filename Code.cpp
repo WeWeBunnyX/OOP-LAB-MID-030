55605e1292fdd2d5dc67095c81e11557dd1079d9
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-03-28 09:51:18 +0500

    initial commit , create Reservation class

--- Code.cpp
+++ Code.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<string>
 
+using namespace std;
+
+class Reservation
+{
+public:
+    string name, phone, time;
+
+    Reservation(string name, string phone, string time)
+    {
+        this->name = name;
+        this->phone = phone;
+        this->time = time;
+    }
+};
