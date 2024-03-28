b446813d6917bab1572f96be81b8cb74240df75a
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-03-28 10:16:10 +0500

    reservation created

--- Code.cpp
+++ Code.cpp
@@ -30,15 +30,42 @@ private:
 
 public:
     void menu()
-    {
+        {
         cout << "Welcome to XYZ restaurant!" << endl;
         cout << "To begin, enter your name: " << endl;
         cin >> name;
         cout << "Profile created!" << endl;
         system("cls")
+         }
+
+        void displayTimeSlots()
+        {
+             cout << "Available time slots are: " << endl;
+            for (int i = 1; i < 9; i++)
+             {
+                 string time = to_string(i) + ":00 PM";
+                 cout << time << endl;
+             }
+
+        }
+
+        void takeReservation()
+        {
+        cout << "Please enter your name: " << endl;
+        getline (cin, name);
+        cout << "Please enter your phone number: " << endl;
+        getline (cin,phone);
+
+        cout << "Please enter the time slot you would like to reserve: " << endl;
+        cin >> time;
+        Reservation reservation(name, phone, time);
+        addReservation(reservation);
+        cout << "Your reservation has been made!" << endl;
     }
 
 
+
+    }
 
 
 
